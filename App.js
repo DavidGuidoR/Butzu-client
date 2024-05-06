f027614f2052ce72462f5c82eeb58b4c8d66cb50
Mariano Garcia <marianogarciagz@gmail.com>
2024-05-05 20:20:59 -0600

    :sparkles: Feat: Add navigations

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import DrawerNavigator from './src/navigation/Drawer/DrawerNavigator'
+import EditItemScreen from './screens/Negocio/EditItemScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator />
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Drawer'
+          component={DrawerNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='EditItemScreen'
+          component={EditItemScreen}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
