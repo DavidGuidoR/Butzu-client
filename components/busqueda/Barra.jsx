674d22605c3f3f90b02b8d40fb26ccc890c37fe0
OscarFuentes2027 <oscarfuentes2027@gmail.com>
2024-05-05 21:35:33 -0600

    finish

--- components/busqueda/Barra.jsx
+++ components/busqueda/Barra.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, Image } from 'react-native';
 
 function BarraDeBusqueda() {
   const [texto, setTexto] = useState('');
+  const [mostrarImagen, setMostrarImagen] = useState(true);
+
+  const handleFocus = () => {
+    setMostrarImagen(false);
+  };
 
   return (
     <View style={styles.container}>
+      
       <TextInput
         style={styles.input}
         onChangeText={setTexto}
         value={texto}
         placeholder="Buscar..."
+        onFocus={handleFocus}
       />
     </View>
   );
@@ -27,16 +34,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#ededed',
     borderRadius: 20,
     borderWidth: 1,
-    borderColor: 0,
-    width: 200, // Define el ancho del input
-    height: 50, // Define la altura del input
-    paddingHorizontal: 10, // Ajusta el padding horizontal dentro del input
+    borderColor: '#ededed',
+    height: 50,
+    paddingHorizontal: 10,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 2,
-    // Sombras para Android
     elevation: 2,
+  },
+  image: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
   },
 });
 
--- navigation/Drawer/DrawerNavigator.jsx
+++ navigation/Drawer/DrawerNavigator.jsx
@@ -1,18 +1,32 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from 'react';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { Image, Text, View } from 'react-native';
 import HomeScreen from '../../screens/Home';
 import NegocioScreen from '../../screens/Negocio';
 import MyTabs from './BottomNavigator';
 
-
 const Drawer = createDrawerNavigator();
 
-function DrawerNavigator() {
-
+function CustomDrawerContent(props) {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Butzu" component={MyTabs} />
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Negocio" component={NegocioScreen} />
+    <DrawerContentScrollView {...props}>
+      <View style={{ backgroundColor: 'black', flexDirection: 'row', alignItems: 'center', paddingVertical: 20, paddingHorizontal: 20, height: 200 }}>
+        <Image source={require('../../assets/ojo.png')} style={{ width: 80, height: 80, marginRight: 10, borderRadius: 50 }} />
+        <View style={{ marginLeft: 10 }}>
+          <Text style={{ color: 'white', fontSize: 27, paddingVertical: 20 }}>Usuario</Text>
+        </View>
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator drawerContent={CustomDrawerContent}>
+      <Drawer.Screen name="Negocios" component={MyTabs} />
+      <Drawer.Screen name="Perfil" component={HomeScreen} />
+      <Drawer.Screen name="Cerrar sesión" component={NegocioScreen} />
     </Drawer.Navigator>
   );
 }
