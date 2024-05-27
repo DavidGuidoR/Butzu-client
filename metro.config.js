469a82ec0e958032865f4b04959f158a30adc521
DavidGuidoR <GuidoD890@gmail.com>
2024-05-27 08:53:18 -0600

    adicion de creacion de usuario

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,19 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-const defaultConfig = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-defaultConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
-defaultConfig.resolver.sourceExts.push('svg');
+  const { transformer, resolver } = config;
 
-module.exports = defaultConfig;
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer")
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
+
+  return config;
+})();
